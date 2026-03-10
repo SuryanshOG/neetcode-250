37381f0debebc618488ed42cd8ab08319092b9f8
Suryansh Shukla <suryanshshukla23@lpu.in>
2026-03-10 17:33:58 +0530

    Solve : Best time to buy and sell stock 2

---
+++ arrays-and-hashing/best-time-to-buy-and-sell-stock-2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    vector<int> prices = {7,1,5,3,6,4};
+    int profit = 0;
+    for (int i = 1; i < prices.size(); i++) {
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+    cout << profit << endl;
+    return 0;
+}
